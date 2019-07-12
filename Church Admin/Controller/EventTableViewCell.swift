4c765f7e84b8eb5be83509909f39cda3e195a1c1
RoneyThomas <roneythomas6@gmail.com>
2019-07-12 02:02:50 -0400

    Autocompletes events

--- Church Admin/Controller/EventTableViewCell.swift
+++ Church Admin/Controller/EventTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EventTableViewCell: UITableViewCell {
+class EventTableViewCell: UITableViewCell, UITextFieldDelegate {
     
     var datePicker = UIDatePicker()
     var toolBar = UIToolbar()
@@ -20,11 +20,11 @@ class EventTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        eventTextField.delegate = self
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
         // Configure the view for the selected state
     }
     
@@ -53,29 +53,28 @@ class EventTableViewCell: UITableViewCell {
         datePicker.removeFromSuperview()
     }
     
-//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        return !autoCompleteText(in: textField, using: string, suggestions: eventSuggestions)
-//    }
-//
-//    func autoCompleteText(in textField: UITextField, using string: String, suggestions: [String]) -> Bool {
-//        if !string.isEmpty,
-//            let selectedTextRange = textField.selectedTextRange, selectedTextRange.end == textField.endOfDocument,
-//            let prefixRange = textField.textRange(from: textField.beginningOfDocument, to: selectedTextRange.start),
-//            let text = textField.text(in: prefixRange) {
-//            let prefix = text + string
-//            let matches = suggestions.filter { $0.hasPrefix(prefix) }
-//
-//            if (matches.count > 0) {
-//                textField.text = matches[0]
-//
-//                if let start = textField.position(from: textField.beginningOfDocument, offset: prefix.count) {
-//                    textField.selectedTextRange = textField.textRange(from: start, to: textField.endOfDocument)
-//
-//                    return true
-//                }
-//            }
-//        }
-//
-//        return false
-//    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return !autoCompleteText(in: textField, using: string, suggestions: eventSuggestions)
+    }
+
+    func autoCompleteText(in textField: UITextField, using string: String, suggestions: [String]) -> Bool {
+        if !string.isEmpty,
+            let selectedTextRange = textField.selectedTextRange, selectedTextRange.end == textField.endOfDocument,
+            let prefixRange = textField.textRange(from: textField.beginningOfDocument, to: selectedTextRange.start),
+            let text = textField.text(in: prefixRange) {
+            let prefix = text + string
+            let matches = suggestions.filter { $0.lowercased().hasPrefix(prefix.lowercased()) }
+
+            if (matches.count > 0) {
+                textField.text = matches[0]
+
+                if let start = textField.position(from: textField.beginningOfDocument, offset: prefix.count) {
+                    textField.selectedTextRange = textField.textRange(from: start, to: textField.endOfDocument)
+
+                    return true
+                }
+            }
+        }
+        return false
+    }
 }
