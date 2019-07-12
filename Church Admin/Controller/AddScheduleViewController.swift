d8ca2a5a3ba4bf9791f795854926a0c264d64010
RoneyThomas <roneythomas6@gmail.com>
2019-07-12 01:37:39 -0400

    Added ability to add events to the AddSchedule view controller

--- Church Admin/Controller/AddScheduleViewController.swift
+++ Church Admin/Controller/AddScheduleViewController.swift
@@ -8,33 +8,40 @@
 
 import UIKit
 
-class AddScheduleViewController: UIViewController {
-
+class AddScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     var datePicker = UIDatePicker()
     var toolBar = UIToolbar()
+    var events: Array<String> = Array()
     
+    @IBOutlet weak var eventTableView: UITableView!
     @IBOutlet weak var titleTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+        events.append("")
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @IBAction func pressedDatePicker(_ sender: Any) {
         print("pressedDatePicker")
         datePicker = UIDatePicker.init()
-        datePicker.backgroundColor = UIColor.white
+        //        datePicker.backgroundColor = UIColor.white
         
         datePicker.autoresizingMask = .flexibleWidth
         datePicker.datePickerMode = .date
@@ -44,7 +51,7 @@ class AddScheduleViewController: UIViewController {
         self.view.addSubview(datePicker)
         
         toolBar = UIToolbar(frame: CGRect(x: 0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
-//        toolBar.barStyle = .blackTranslucent
+        //        toolBar.barStyle = .blackTranslucent
         toolBar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.onDoneButtonClick))]
         toolBar.sizeToFit()
         self.view.addSubview(toolBar)
@@ -63,5 +70,34 @@ class AddScheduleViewController: UIViewController {
     @objc func onDoneButtonClick() {
         toolBar.removeFromSuperview()
         datePicker.removeFromSuperview()
+    }
+    
+    func configureTableView() {
+        eventTableView.rowHeight = UITableView.automaticDimension
+        eventTableView.estimatedRowHeight = 43.5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "Cell",
+            for: indexPath) as! EventTableViewCell
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            events.remove(at: indexPath.row)
+            eventTableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func addEventRow(_ sender: Any) {
+        events.append("")
+        eventTableView.reloadData()
     }
 }
---
+++ Church Admin/Controller/EventTableViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  EventTableViewCell.swift
+//  Church Admin
+//
+//  Created by Roney Thomas Mannoocheril on 2019-07-11.
+//  Copyright © 2019 Roney Thomas Mannoocheril. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    
+    var datePicker = UIDatePicker()
+    var toolBar = UIToolbar()
+    var eventSuggestions = ["Prabhatha Namaskaram", "Holy Qurbana", "Sunday School"]
+    
+    @IBOutlet weak var timeTextField: UITextField!
+    @IBOutlet weak var eventTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func timeFieldEditing(_ sender: UITextField) {
+        datePicker = UIDatePicker.init()
+        
+        datePicker.autoresizingMask = .flexibleWidth
+        datePicker.datePickerMode = .time
+        
+        sender.inputView = datePicker
+        datePicker.addTarget(self, action: #selector(self.timeChanged(_:)), for: .valueChanged)
+    }
+    
+    @objc func timeChanged(_ sender: UIDatePicker?) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm a"
+        
+        if let date = sender?.date {
+            print("Picked the date \(dateFormatter.string(from: date))")
+            self.timeTextField.text = dateFormatter.string(from: date)
+        }
+    }
+    
+    @objc func onDoneButtonClick() {
+        toolBar.removeFromSuperview()
+        datePicker.removeFromSuperview()
+    }
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        return !autoCompleteText(in: textField, using: string, suggestions: eventSuggestions)
+//    }
+//
+//    func autoCompleteText(in textField: UITextField, using string: String, suggestions: [String]) -> Bool {
+//        if !string.isEmpty,
+//            let selectedTextRange = textField.selectedTextRange, selectedTextRange.end == textField.endOfDocument,
+//            let prefixRange = textField.textRange(from: textField.beginningOfDocument, to: selectedTextRange.start),
+//            let text = textField.text(in: prefixRange) {
+//            let prefix = text + string
+//            let matches = suggestions.filter { $0.hasPrefix(prefix) }
+//
+//            if (matches.count > 0) {
+//                textField.text = matches[0]
+//
+//                if let start = textField.position(from: textField.beginningOfDocument, offset: prefix.count) {
+//                    textField.selectedTextRange = textField.textRange(from: start, to: textField.endOfDocument)
+//
+//                    return true
+//                }
+//            }
+//        }
+//
+//        return false
+//    }
+}
